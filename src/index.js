fab3ea832c90b2ac2cc1c3d4f4630daae1551808
Husain Batatwala <batatawala.husain@gmail.com>
2022-01-02 01:01:02 +0530

    modified env variable PORt

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const port = process.env.RECIPE_BOOK_APP_PORT;
+const port = process.env.PORT;
 require("./db/mongoose");
 
 const userRouter = require("./routers/user");
